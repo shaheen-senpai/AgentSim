df8b7c5e5ea8df7fc89c2dd0c23962275dd035aa
Shaheen <shaheen50053@gmail.com>
2026-09-16 01:35:00 +0530

    gg

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ["applaud-foothill-dash.ngrok-free.dev"]
 };
 
 export default nextConfig;
